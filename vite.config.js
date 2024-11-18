b8ff5f05d0b6862592d3107a8572d94bd4cc698a
sai <1430792918@qq.com>
2024-11-18 14:57:14 +0800

    增加打包分析包，分析打包文件结构，方便提升打包后的访问效率

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,32 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { visualizer } from 'rollup-plugin-visualizer'
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   const proxyPrefix = env.VITE_APP_PROXY_PREFIX
 
   return defineConfig({
     base: env.VITE_APP_BASE,
-    plugins: [vue(), vueJsx()],
+    plugins: [
+      vue(),
+      vueJsx(),
+      visualizer({
+        emitFile: false,
+        file: 'stats.html', //分析图生成的文件名
+        open: true //如果存在本地服务端口，将在打包后自动展示
+      })
+    ],
     resolve: {
       alias: {
         '@': resolve(__dirname, 'src'),
         '@cps': resolve(__dirname, 'src/components'),
-        'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
-      },
+        'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js'
+      }
     },
 
     build: {
-      chunkSizeWarningLimit: 1500,
+      chunkSizeWarningLimit: 1500
       // rollupOptions: {
       //   output: {
       //     manualChunks(id) {
@@ -39,9 +48,9 @@ export default ({ mode }) => {
           changeOrigin: true,
           ws: true,
           toProxy: true,
-          rewrite: path => path.replace(new RegExp(`^${proxyPrefix}`), ''),
-        },
-      },
-    },
+          rewrite: (path) => path.replace(new RegExp(`^${proxyPrefix}`), '')
+        }
+      }
+    }
   })
 }
