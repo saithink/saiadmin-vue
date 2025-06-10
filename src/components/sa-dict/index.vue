c13a3084aedd0030111dc7e1ceddfc0ad2d344e0
sai <1430792918@qq.com>
2025-06-10 09:24:20 +0800

    dict解析增加，可以解析数组数据

--- src/components/sa-dict/index.vue
+++ src/components/sa-dict/index.vue
@@ -2,17 +2,37 @@
   <div>
     <!-- 渲染 span -->
     <span v-if="props.render === 'span'">
-      {{ tool.getLabel(value, props.options.length > 0 ? props.options : dictList[props.dict]) }}
+      <template v-if="Array.isArray(value)">
+        {{
+          value.map((v) => tool.getLabel(v, props.options.length > 0 ? props.options : dictList[props.dict])).join(', ')
+        }}
+      </template>
+      <template v-else>
+        {{ tool.getLabel(value, props.options.length > 0 ? props.options : dictList[props.dict]) }}
+      </template>
     </span>
     <!-- 渲染 tag -->
-    <a-tag
-      v-if="props.render === 'tag' && value !== ''"
-      :color="
-        tool.getColor(value, props.options.length > 0 ? props.options : dictList[props.dict], props.colors || [])
-      ">
-      {{ tool.getLabel(value, props.options.length > 0 ? props.options : dictList[props.dict]) }}
-    </a-tag>
-    <span v-else></span>
+    <template v-if="props.render === 'tag'">
+      <template v-if="Array.isArray(value)">
+        <a-tag
+          v-for="(v, index) in value"
+          :key="index"
+          class="mr-2"
+          :color="
+            tool.getColor(v, props.options.length > 0 ? props.options : dictList[props.dict], props.colors || [])
+          ">
+          {{ tool.getLabel(v, props.options.length > 0 ? props.options : dictList[props.dict]) }}
+        </a-tag>
+      </template>
+      <a-tag
+        v-else-if="value !== ''"
+        :color="
+          tool.getColor(value, props.options.length > 0 ? props.options : dictList[props.dict], props.colors || [])
+        ">
+        {{ tool.getLabel(value, props.options.length > 0 ? props.options : dictList[props.dict]) }}
+      </a-tag>
+      <span v-else></span>
+    </template>
   </div>
 </template>
 
@@ -25,7 +45,7 @@ const dictList = useDictStore().data
 const value = ref()
 
 const props = defineProps({
-  value: { type: [String, Number] },
+  value: { type: [String, Number, Array] },
   render: { type: String, default: 'tag' },
   dict: { type: String, default: '' },
   options: { type: Array, default: [] },
