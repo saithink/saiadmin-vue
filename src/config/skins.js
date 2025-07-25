ca8c285aa307f1460821188ce0c0c12c1ff89f80
sai <1430792918@qq.com>
2025-07-25 09:59:14 +0800

    皮肤资源文件调整到assets目录

--- src/config/skins.js
+++ src/config/skins.js
@@ -1,18 +1,23 @@
+import mine from '@/assets/skins-thumb/mine/thumb.jpg'
+import classics from '@/assets/skins-thumb/classics/thumb.jpg'
+import businessGray from '@/assets/skins-thumb/businessGray/thumb.jpg'
+import city from '@/assets/skins-thumb/city/thumb.jpg'
+
 export default [
   {
     name: 'mine',
-    thumb: 'skins-thumb/mine/thumb.jpg',
+    thumb: mine
   },
   {
     name: 'classics',
-    thumb: 'skins-thumb/classics/thumb.jpg',
+    thumb: classics
   },
   {
     name: 'businessGray',
-    thumb: 'skins-thumb/businessGray/thumb.jpg',
+    thumb: businessGray
   },
   {
     name: 'city',
-    thumb: 'skins-thumb/city/thumb.jpg',
+    thumb: city
   }
 ]
--- src/layout/components/components/skin.vue
+++ src/layout/components/components/skin.vue
@@ -6,14 +6,13 @@
         v-for="(item, index) in skinList"
         :key="item.name"
         :class="index === 0 ? '' : 'mt-3'"
-        :body-style="{ width: '100%', display: 'flex', justifyContent: 'space-between', padding: '10px' }"
-      >
+        :body-style="{ width: '100%', display: 'flex', justifyContent: 'space-between', padding: '10px' }">
         <a-row class="w-full flex items-center">
           <a-col :span="3" class="flex flex-col text-center">
             <div class="leading-6">{{ $t(`skin.${item.name}`) }}</div>
           </a-col>
           <a-col :span="6" class="flex flex-col text-center">
-            <a-image :src="$url + item.thumb" class="rounded border" />
+            <a-image :src="item.thumb" class="rounded border" />
           </a-col>
           <a-col :span="12" class="flex items-center pl-3 pr-3">
             {{ $t(`skin.${item.name}Desc`) }}
@@ -22,8 +21,7 @@
             <a-button
               :type="appStore.skin === item.name ? 'primary' : 'secondary'"
               :disabled="appStore.skin === item.name"
-              @click="useSkin(item.name)"
-            >
+              @click="useSkin(item.name)">
               {{ appStore.skin === item.name ? $t('skin.activated') : $t('skin.use') }}
             </a-button>
           </a-col>
@@ -41,8 +39,8 @@ import skins from '@/config/skins'
 const visible = ref(false)
 const appStore = useAppStore()
 
-const open = () => visible.value = true
-const close = () => visible.value = false
+const open = () => (visible.value = true)
+const close = () => (visible.value = false)
 
 const useSkin = (name) => appStore.useSkin(name)
 
@@ -51,5 +49,4 @@ const skinList = reactive(skins)
 defineExpose({ open })
 </script>
 
-<style scoped lang="less">
-</style>
+<style scoped lang="less"></style>
