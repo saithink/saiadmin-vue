ebd80179fdb6730de93ae61daeb6748a36efa55d
sai <1430792918@qq.com>
2025-07-25 23:41:02 +0800

    优化打包

--- vite.config.js
+++ vite.config.js
@@ -27,16 +27,20 @@ export default ({ mode }) => {
     },
 
     build: {
-      chunkSizeWarningLimit: 1500
-      // rollupOptions: {
-      //   output: {
-      //     manualChunks(id) {
-      //       if (id.includes('node_modules')) {
-      //         return id.toString().split("node_modules/")[1].split("/")[0].toString();
-      //       }
-      //     }
-      //   }
-      // }
+      chunkSizeWarningLimit: 1500,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id
+                .toString()
+                .split('node_modules/')[1]
+                .split('/')[0]
+                .toString()
+            }
+          }
+        }
+      }
     },
 
     server: {
